b98054a3254faaa97f3bf9c35ad18eb009564549
Bruno Rocha <rochacbruno@gmail.com>
2013-03-28 02:35:21 +0000

    urls

--- opps/promos/urls.py
+++ opps/promos/urls.py
@@ -2,3 +2,18 @@
 # -*- coding: utf-8 -*-
 #
 from django.conf.urls import patterns, url
+
+from .views import PromoDetail, PromoList, ChannelPromoList
+
+
+urlpatterns = patterns(
+    '',
+    url(r'^$', PromoList.as_view(), name='list_promo'),
+    url(r'^channel/(?P<channel__long_slug>[\w//-]+)$', ChannelPromoList.as_view(),
+        name='channel_promo'),
+    url(r'^(?P<slug>[\w-]+)/(?P<result>[\w-]+)$',
+        PromoDetail.as_view(), name='result_promo'),
+    url(r'^(?P<slug>[\w-]+)$',
+        PromoDetail.as_view(), name='open_promo'),
+
+)
