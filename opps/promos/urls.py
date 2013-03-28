affdb0ec1f33184a82f449e4cd704789bb875381
Bruno Rocha <rochacbruno@gmail.com>
2013-03-28 02:39:23 +0000

    fix namespace in urls

--- opps/promos/urls.py
+++ opps/promos/urls.py
@@ -8,7 +8,7 @@ from .views import PromoDetail, PromoList, ChannelPromoList
 
 urlpatterns = patterns(
     '',
-    url(r'^$', PromoList.as_view(), name='list_promo'),
+    url(r'^$', PromoList.as_view(), name='list_promos'),
     url(r'^channel/(?P<channel__long_slug>[\w//-]+)$', ChannelPromoList.as_view(),
         name='channel_promo'),
     url(r'^(?P<slug>[\w-]+)/(?P<result>[\w-]+)$',
