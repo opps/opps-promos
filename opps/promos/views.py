7d2bc085cf43dffd502d1e6f14c3cd9307f2bcf9
Rael Max <contato@raelmax.com>
2013-08-02 17:17:08 +0000

    fix: ordering

--- opps/promos/views.py
+++ opps/promos/views.py
@@ -53,7 +53,7 @@ class PromoList(ListView):
 
     @property
     def queryset(self):
-        return Promo.objects.all_published()
+        return Promo.objects.all_published().order_by('-date_end')
 
 
 class ChannelPromoList(ListView):
