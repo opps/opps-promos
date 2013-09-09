2facefbfac150545356f73439bdb42cb5a608d0e
Hersonls <hersonls@gmail.com>
2013-09-09 02:57:34 -0300

    Added exclude feature in get_promos_by tag

--- opps/promos/templatetags/promos_tags.py
+++ opps/promos/templatetags/promos_tags.py
@@ -9,13 +9,14 @@ register = template.Library()
 
 
 @register.assignment_tag
-def get_promos_by(opened=True, **filters):
+def get_promos_by(opened=True, exclude=False, **filters):
     """
         Return a list of promos filtered by given args
 
         Usage:
 
-          {% get_promos_by opened=[True|False] filter1=value filter2=value .. %}
+          {% get_promos_by opened=[True|False] exclude=[True|False]
+             filter1=value filter2=value .. %}
     """
 
     qs = Promo.objects.all_opened()
@@ -23,5 +24,9 @@ def get_promos_by(opened=True, **filters):
     if not opened:
         qs = Promo.objects.all_closed()
 
-    return qs.filter(site=settings.SITE_ID,
-                     **filters)
+    qs = qs.filter(site=settings.SITE_ID)
+
+    if exclude:
+        return qs.exclude(**filters)
+
+    return qs.filter(**filters)
